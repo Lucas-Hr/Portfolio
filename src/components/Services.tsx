3a0eb9581006d114457dd63e7d2c38c10f293eaa
Lucas-Hr <lucashenintsoarandriamiarisoa@gmail.com>
2025-08-04 20:56:16 +0300

    Added pagination, fixed animation, fixed navbar

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -9,11 +9,13 @@ function Services() {
         <motion.h2 
         initial={{ opacity: 0, y: 50 }}
         whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: true }}
         transition={{ duration: 0.5 }}
         className='text-xl md:text-2xl text-[#2AD883] font-bold text-center'>Services</motion.h2>
         <motion.div
         initial={{ opacity: 0, y: 50 }}
         whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: true }}
         transition={{ duration: 0.5, delay: 0.5 }}
         className='flex flex-col md:flex-row justify-between my-10'>
             <CardServices image='/uiux.svg' title="UI/UX Design" description="Define, Research, Analysis & Planning,Design, Prototyping, Testing"/>
